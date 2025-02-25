bdfea8762a5d362b1881a7bac1c42f9f26e0ee92
latronixo <pamerra@mail.ru>
2025-02-25 21:41:42 +0300

    Удалил сториборд, сделал gitignore, api вписал в Constants.swift

---
+++ 166.HowMuchDoesBitcoinCost-14-module/Constants.swift
@@ -0,0 +1,20 @@
+//
+//  Constants.swift
+//  142.Clima-13-module
+//
+//  Created by Валентин Картошкин on 20.02.2025.
+//
+
+import Foundation
+
+enum Constants {
+//    static let background = "background"
+//    static let weatherColor = "weatherColor"
+//    static let celsius = "°C"
+//    static let searchSF = "magnifyingglass" //картинка в виде лупы для кнопки поиска, изображение будет браться из SF Symbols
+//    static let geoSF = "location.circle.fill" //картинка для кнопки определения геопозиции, изображение будет браться из SF Symbols
+//    static let search = "Search"
+//    static let conditionSF = "cloud.rain"
+    static let id = "ec22b65c-552f-452c-a1d1-0a11dcaceecd"  // мой id на сайте coinapi.io
+    
+}
