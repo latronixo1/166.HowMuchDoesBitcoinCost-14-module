dc0c7147bd449b7493cb4f8087f99fca7b46d433
latronixo <pamerra@mail.ru>
2025-02-25 21:28:26 +0300

    Initial Commit

---
+++ 166.HowMuchDoesBitcoinCost-14-module/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  166.HowMuchDoesBitcoinCost-14-module
+//
+//  Created by Валентин Картошкин on 25.02.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
